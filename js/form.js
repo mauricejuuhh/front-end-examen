7fff5a350cb56d8639477cc825cdb922946c5b66
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2018-06-11 13:20:54 +0200

    vorige en next buttons toegevoegt
    De buttons next en vorige zijn toegevoegt plus er zijn nog een aantal functionaliteiten toegevoegt zoals als je de leerlings naam invult dat je de naam boven aan in het menu ziet

--- js/form.js
+++ js/form.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 
+    //checked on wich button you clicked and go's to that page number
     $('.menu-button').click(function () {
         $clickedNavElement = $('.menu-button').index(this);
         $GoToPage($clickedNavElement);
     });
 
-    $navClickable = 1;
+    //Go to spesific page function
     $GoToPage = function ($pageNumber) {
 
-        if ($currentPage != $pageNumber && $navClickable == 1) {
+        if ($currentPage !== $pageNumber && $navClickable === 1) {
             $navClickable = 0;
 
             $('.form_page').fadeOut(250);
@@ -21,25 +22,84 @@ $(document).ready(function() {
             }, 500);
         }
 
-        $currentPage = $pageNumber;
+        
+        if ($pageNumber === 0 || $pageNumber <= 0) {
+            $("#previous").addClass("disabled");
+        } else {
+            $("#previous").removeClass("disabled");
+        }
 
+
+        if ($pageNumber === $totalPages || $pageNumber >= $totalPages) {
+            $("#next").addClass("disabled");
+        } else {
+            $("#next").removeClass("disabled");
+        }
+
+        
+        $currentPage = $pageNumber;
     };
 
-    $currentPage = 1;
+    $navClickable = 1;
+    $currentPage = null;
+    $totalPages = $(".menu-button").length - 1;
+
+    //Start page
     $GoToPage(0);
 
 
+    
+//All code for NEXT click button
+
+    //Next button function - Go's to the next page
     $('#next').click(function () {
         $newpage = $currentPage + 1;
-        if ($newpage <= $currentPage && $navClickable == 1){
+        if ($newpage >= $currentPage && $navClickable === 1){
             $GoToPage($newpage);
         }
     });
 
+
+
+//All code for PREVIOUS click button
+
+    //Previous button function - Go's to the previous page
     $('#previous').click(function () {
-        $clickedNavElement = $('.menu-button').index(this);
-        $GoToPage($clickedNavElement);
+        $newpage = $currentPage - 1;
+        if ($newpage <= $currentPage && $navClickable === 1){
+            $GoToPage($newpage);
+        }
     });
+
+
+// All code for nav menu
+
+    // nav menu top avatar image
+    $changeImage = function(input) {
+        if (input.files && input.files[0]) {
+            let reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.user-image').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    };
+
+    $("#uploadImage").change(function(){
+        $changeImage(this);
+    });
+
+
+
+    // nav menu top name
+    $('#Form_firstname').change(function () {
+        $("#nav-name").fadeOut(function () {
+            $(this).text($('#Form_firstname').val());
+        }).fadeIn();
+    });
+
 
 
 
